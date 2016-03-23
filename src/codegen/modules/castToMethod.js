09601c5062f48d43bb0e0b20aa465eba5b69747f
Luqiang Tian <Luqiangtian@gmail.com>
2016-03-23 12:30:26 +0800

    Add CodGen for CastTo method

---
+++ src/codegen/modules/castToMethod.js
@@ -0,0 +1,95 @@
+
+var config = require('../config');
+
+var CastToMethod='\
+        /// <summary>\n\
+        /// Cast an entity of type {0} to its derived type {2}\n\
+        /// </summary>\n\
+        /// <param name="source">source entity</param>\n\
+        public static {3} CastTo{1}(this global::Microsoft.OData.Client.DataServiceQuerySingle<{0}> source)\n\
+        {\n\
+            global::Microsoft.OData.Client.DataServiceQuerySingle<{2}> query = source.CastTo<{2}>();\n\
+            return new {3}(source.Context, query.GetPath(null));\n\
+        }\n';
+
+exports.generateCastTo = gnerate;
+function gnerate(model)
+{ 
+    var result='';
+    for(var i=0;i<model.types.length;i++)
+    {
+        var entityType = model.types[i];
+        var current = entityType.baseType;
+        while (current != undefined && current!='' && isEntityType(model, current))
+        {
+            var baseTypeName = current;
+            baseTypeName = GetPrefixedFullName(config.Constants.Code.DefaultNamespace, GetFixedName(baseTypeName));
+            var entityTypeFullName = GetPrefixedFullName(config.Constants.Code.DefaultNamespace, GetFixedName(entityType.name));
+            var returnTypeName = GetPrefixedFullName(config.Constants.Code.DefaultNamespace, GetFixedName(entityType.name + 'Single'));
+            result+=CastToMethod.format(baseTypeName, entityType.name[0].toUpperCase() + entityType.name.substr(1), entityTypeFullName, returnTypeName);
+            
+            baseTypeName = current;
+            current = undefined;
+            for(var j=0;j<model.types.length;j++){
+                if(model.types[j].name === baseTypeName && !!model.types[j].baseType){
+                    current = model.types[j].baseType;
+                }
+            };
+        }
+    }
+    return result;
+}
+
+function GetPrefixedFullName(namespace, fixedName)
+{
+    return 'global::' + namespace + '.' +fixedName;
+}
+
+var LanguageKeywords = [
+    'abstract', 'as', 'base', 'byte', 'bool', 'break', 'case', 'catch', 'char', 'checked', 'class', 'const', 'continue',
+    'decimal', 'default', 'delegate', 'do', 'double', 'else', 'enum', 'event', 'explicit', 'extern', 'false', 'for',
+    'foreach', 'finally', 'fixed', 'float', 'goto', 'if', 'implicit', 'in', 'int', 'interface', 'internal', 'is', 'lock',
+    'long', 'namespace', 'new', 'null', 'object', 'operator', 'out', 'override', 'params', 'private', 'protected', 'public',
+    'readonly', 'ref', 'return', 'sbyte', 'sealed', 'string', 'short', 'sizeof', 'stackalloc', 'static', 'struct', 'switch',
+    'this', 'throw', 'true', 'try', 'typeof', 'uint', 'ulong', 'unchecked', 'unsafe', 'ushort', 'using', 'virtual', 'volatile',
+    'void', 'while'];
+
+function GetFixedName(originalName)
+{
+    var fixedName = originalName;
+
+    if (_.includes(LanguageKeywords,fixedName))
+    {
+        fixedName = '@' + fixedName;
+    }
+
+    return fixedName;
+}
+
+function isEntityType(model, typeName)
+{
+    var returnValue=false;
+    for (var i=0; i<model.types.length; i++)
+    {
+        if(model.types[i].name != typeName)
+        {
+            continue;
+        }
+
+        for(var j=0;j< model.types[i].properties.length;j++)
+        {
+            if(!!(model.types[i].properties[j].isKey))
+            { returnValue = true; break; }
+        }
+        if(returnValue) {
+            break;
+        } else {
+            if(model.types[i].baseType)
+            {
+                return isEntityType(model, model.types[i].baseType);
+            }
+        }
+    }
+
+    return returnValue;
+}
