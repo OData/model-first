893beb1e56857b76bfa8f4df94dd463ad86b0dd0
Baoming520 <dxl926@gmail.com>
2016-04-11 19:56:23 +0800

    update

--- src/codegen/utilities/zipper.js
+++ src/codegen/utilities/zipper.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var EasyZip = require('easy-zip').EasyZip;
 var StringHelper = require('../helpers/stringhelper');
 var Logger = require('./logger');
-var logger = Logger.getInstance();
 
 /*
 ** Zip the whole folder to a package.
@@ -12,6 +11,7 @@ var logger = Logger.getInstance();
 **     callback: The callback function.
 */
 exports.zipFolder = function(folderPath, zipPath, callback){
+    var logger = Logger.getInstance();
     try{
         var easyZip = traverseFolder(folderPath);
         easyZip.writeToFile(zipPath);
@@ -33,7 +33,7 @@ exports.zipFolder = function(folderPath, zipPath, callback){
 **     returns an EasyZip instance. 
 */
 function traverseFolder(folderPath, easyZip, folder){
-    if(null === easyZip){
+    if(!easyZip){
         easyZip = new EasyZip();
     }
     var folderName = StringHelper.getLastSegment(folderPath);
